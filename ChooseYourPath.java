9d7630c7c8db9d422c0cf374f2b4f16e70e7ef34
LucasZMrSFPer4 <lucas.zhang26@student.tamdistrict.org>
2024-10-26 22:37:08 -0700

    git commit

--- ChooseYourPath.java
+++ ChooseYourPath.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class ChooseYourPath
 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Choose one of the 3 doors: Blue, Red, Green: ");
+        String doorOfChoice = sc.nextLine();
+
+        if (doorOfChoice.equals("Blue"))
+        {
+            System.out.print("Are you tired?");
+            String s1 = sc.nextLine();
+        }
     }
 }
