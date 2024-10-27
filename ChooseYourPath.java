40e48816ae3f7376df6fb961c143f09d6729a5ca
LucasZMrSFPer4 <lucas.zhang26@student.tamdistrict.org>
2024-10-27 14:42:01 -0700

    git commit

--- ChooseYourPath.java
+++ ChooseYourPath.java
@@ -39,5 +39,65 @@ public class ChooseYourPath
                 }
             }
         }
+
+        else if (doorOfChoice.equals("Red"))
+        {
+            System.out.print("Are you thirsty? (true or false): ");
+            boolean b1 = Boolean.parseBoolean(sc.nextLine());
+
+            if (!b1)
+            {
+                System.out.print("You've decided to go home.");
+            }
+            else
+            {
+                System.out.print("You've decided to go to the bar.");
+                System.out.print("Alcohol Choices: \n1. Beer\n2. Wine\n3. Tequila\n");
+                String actionOfChoice = sc.nextLine();
+
+                if (actionOfChoice.equals("1"))
+                {
+                    System.out.print("You've decided to drink beer.");
+                }
+                else if (actionOfChoice.equals("2"))
+                {
+                    System.out.print("You've decided to drink wine.");
+                }
+                else
+                {
+                    System.out.print("You've decided to drink tequila.");
+                }
+            }
+        }
+
+        else
+        {
+            System.out.print("Are you hungry for money? (true or false): ");
+            boolean b1 = Boolean.parseBoolean(sc.nextLine());
+
+            if (!b1)
+            {
+                System.out.print("You've decided to go home.");
+            }
+            else
+            {
+                System.out.print("You've decided to go to the casino.");
+                System.out.print("Game Choices: \n1. Blackjack\n2. Poker\n3. Roulette\n");
+                String actionOfChoice = sc.nextLine();
+
+                if (actionOfChoice.equals("1"))
+                {
+                    System.out.print("You've decided to play Blackjack.");
+                }
+                else if (actionOfChoice.equals("2"))
+                {
+                    System.out.print("You've decided to play Poker.");
+                }
+                else
+                {
+                    System.out.print("You've decided to play Roulette.");
+                }
+            }
+        }
     }
 }
